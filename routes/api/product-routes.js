19e6e4d055602ab374aff46ef95a588808007de8
Claudia Correa Salhab <claudiacorreasalhab@gmail.com>
2021-08-19 19:22:42 -0400

    added api route for product get /

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -1,22 +1,32 @@
-const router = require('express').Router();
-const { Product, Category, Tag, ProductTag } = require('../../models');
+const router = require("express").Router();
+const { Product, Category, Tag, ProductTag } = require("../../models");
 
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get("/", async (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  const productData = await Product.findAll({ include: [Category, Tag] });
+  res.json(productData);
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get("/:id", async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  const productIdData = await Product.findByPk(req.params.id, {
+    include: [{ model: Category }, { model: Tag }],
+  }).catch((err) => res.status(500).json(err));
+  if (!productIdData)
+    return res
+      .status(404)
+      .json({ message: `There is no product by the ID of ${req.params.id}` });
+  res.status(200).json(productIdData);
 });
 
 // create new product
-router.post('/', (req, res) => {
+router.post("/", (req, res) => {
   /* req.body should look like this...
     {
       product_name: "Basketball",
@@ -48,7 +58,7 @@ router.post('/', (req, res) => {
 });
 
 // update product
-router.put('/:id', (req, res) => {
+router.put("/:id", (req, res) => {
   // update product data
   Product.update(req.body, {
     where: {
@@ -89,8 +99,20 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete("/:id", async (req, res) => {
   // delete one product by its `id` value
+  const deletedProduct = await Product.destroy({
+    where: { id: req.params.id },
+  });
+  if (!deletedProduct) {
+    return res.status(404).json({
+      error_message:
+        "Product cannot be deleted because the product does not exist.",
+    });
+  }
+  res
+    .status(200)
+    .json({ message: "The product has been successfully deleted." });
 });
 
 module.exports = router;
